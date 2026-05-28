3699a99baeb0b4103ca5057744f66ad5a3f3c98d
Akhumeya Abiodun <chrisjone007@gmail.com>
2026-05-28 15:54:22 +0100

    Recovered project files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import About from './components/About'
+import Contact from './components/Contact'
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Hero />
+      <Projects />
+      <Skills />
+      <About />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
